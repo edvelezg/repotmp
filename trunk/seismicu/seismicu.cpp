9d1c766bce8b3f01ba261c6c1a4e06ac86a4ec1f
EdVelez.G <EdVelez.G@c7d15216-b18b-11dd-89d2-dfb2a00067cc>
2008-11-16 15:05:18 +0000

    Run doesn't close dialog

---
+++ trunk/seismicu/seismicu.cpp
@@ -0,0 +1,84 @@
+#include <QtGui>
+#include "seismicu.h"
+
+SeismicU::SeismicU(QWidget *parent)
+    : QDialog(parent)
+{
+    setupUi(this);
+
+    QPushButton *convertButton =
+            buttonBox->button(QDialogButtonBox::Ok);
+    convertButton->setText(tr("&Run"));
+    convertButton->setEnabled(true);
+    outputTextEdit->append("Lol" );
+
+    connect(convertButton, SIGNAL(clicked()),
+            this, SLOT(runScript()));
+//  connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    connect(&process, SIGNAL(readyReadStandardOutput()),
+            this, SLOT(updateOutputTextEdit()));
+    connect(&process, SIGNAL(finished(int, QProcess::ExitStatus)),
+            this, SLOT(processFinished(int, QProcess::ExitStatus)));
+    connect(&process, SIGNAL(error(QProcess::ProcessError)),
+            this, SLOT(processError(QProcess::ProcessError)));
+}
+
+void SeismicU::runScript()
+{
+    outputTextEdit->clear();
+	
+	QFile file("script.txt");
+	if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
+		return;
+	// I just added this line to test some things 
+	QTextStream out(&file);
+	out << "#!/bin/bash\n";
+	out << "echo Thunder" << endl;
+
+    QStringList args;
+	args << "script.txt";
+
+//  if (enhanceCheckBox->isChecked())
+//      args << "-enhance";
+//  if (monochromeCheckBox->isChecked())
+//      args << "-monochrome";
+	
+    outputTextEdit->append(args.at(0));
+    outputTextEdit->append(process.workingDirectory());
+	QStringList env = QProcess::systemEnvironment();
+	process.setEnvironment(env);
+	env << "CWPROOT=/opt/SU"; 
+	env << "PATH=$PATH:/opt/SU/bin"; 
+	process.start("sh", args);
+	// process.start("/Users/Naix/Tmp/viper-sugui/script.sh");
+//	process.start("/opt/SU/src/demos/Synthetic/Finite_Difference/Sufdmod2/XDemo3");
+}
+
+void SeismicU::processFinished(int exitCode,
+                                    QProcess::ExitStatus exitStatus)
+{
+    if (exitStatus == QProcess::CrashExit) {
+        outputTextEdit->append(tr("Conversion program crashed"));
+    } else if (exitCode != 0) {
+        outputTextEdit->append(QString("Conversion failed, code=%1").arg(exitCode));
+    } else {
+        outputTextEdit->append(tr("File %1 created").arg(targetFile));
+    }
+    buttonBox->button(QDialogButtonBox::Ok)->setEnabled(true);
+}
+
+void SeismicU::processError(QProcess::ProcessError error)
+{
+    if (error == QProcess::FailedToStart) {
+        outputTextEdit->append(tr("Conversion program not found"));
+        buttonBox->button(QDialogButtonBox::Ok)->setEnabled(true);
+    }
+}
+
+void SeismicU::updateOutputTextEdit()
+{
+    QByteArray newData = process.readAllStandardOutput();
+    QString text = outputTextEdit->toPlainText()
+                   + QString::fromLocal8Bit(newData);
+    outputTextEdit->setPlainText(text);
+}
