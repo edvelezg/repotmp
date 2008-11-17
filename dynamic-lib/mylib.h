b211bbc1aa2e27689d4cf7b93686ad9164f80e86
EdVelez.G <EdVelez.G@c7d15216-b18b-11dd-89d2-dfb2a00067cc>
2008-11-17 13:54:07 +0000

    Added dynamic-lib example

---
+++ dynamic-lib/mylib.h
@@ -0,0 +1,15 @@
+#ifndef mylib_h 
+#define mylib_h
+class A;
+class A 
+	{ 
+	public: 
+		A(); 
+		~A() {}; 
+		A(int newa); 
+		int getValue() const; 
+		void setValue(int newa); 
+	private: 
+		int a; 
+	};
+#endif 
