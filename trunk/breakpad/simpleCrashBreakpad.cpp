bd21afb77cba7f4eeb8783558371126a1dd7494f
EdVelez.G <EdVelez.G@c7d15216-b18b-11dd-89d2-dfb2a00067cc>
2008-12-09 18:20:55 +0000

    Breakpad Stuff

---
+++ trunk/breakpad/simpleCrashBreakpad.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+ 
+#include "client/linux/handler/exception_handler.h"
+#include "client/linux/handler/linux_thread.h"
+ 
+ 
+using namespace google_breakpad;
+ 
+// Callback when minidump written.
+static bool MinidumpCallback(const char *dump_path,
+                             const char *minidump_id,
+                             void *context,
+                             bool succeeded) {
+  printf("%s is dumped\n", minidump_id);
+ 
+  return succeeded;
+}
+
+void callMe() {
+  int* foo = NULL;
+  *foo = 42;
+} 
+
+
+void callMeFoo() {
+  callMe();
+}
+
+int main(int argc, char *argv[]) {
+  ExceptionHandler handler_process(".", NULL, MinidumpCallback,
+                           NULL, true);
+  
+  callMeFoo();
+  return 0;
+} 
+
